bac26eacc35adea189f361de6f96082df7d86ad4
benjaminwan <benjaminwan@163.com>
2021-04-08 09:03:31 +0800

    edit build script

--- build-default.bat
+++ build-default.bat
@@ -21,24 +21,6 @@ cmake --build . --config Release -j %NUMBER_OF_PROCESSORS%
 cmake --build . --config Release --target install
 popd
 
-mkdir win-bin-x64-gpu
-pushd win-bin-x64-gpu
-cmake -T "v141,host=x64" -A "x64" ^
-  -DCMAKE_INSTALL_PREFIX=install ^
-  -DCMAKE_BUILD_TYPE=Release -DOCR_OUTPUT="EXE" -DOCR_VULKAN=ON ..
-cmake --build . --config Release -j %NUMBER_OF_PROCESSORS%
-cmake --build . --config Release --target install
-popd
-
-mkdir win-bin-x86-gpu
-pushd win-bin-x86-gpu
-cmake -T "v141,host=x64" -A "Win32" ^
-  -DCMAKE_INSTALL_PREFIX=install ^
-  -DCMAKE_BUILD_TYPE=Release -DOCR_OUTPUT="EXE" -DOCR_VULKAN=ON ..
-cmake --build . --config Release -j %NUMBER_OF_PROCESSORS%
-cmake --build . --config Release --target install
-popd
-
 mkdir win-jni-x64-cpu
 pushd win-jni-x64-cpu
 cmake -T "v141,host=x64" -A "x64" ^
@@ -53,6 +35,24 @@ pushd win-jni-x86-cpu
 cmake -T "v141,host=x64" -A "Win32" ^
   -DCMAKE_INSTALL_PREFIX=install ^
   -DCMAKE_BUILD_TYPE=Release -DOCR_OUTPUT="JNI" -DOCR_VULKAN=OFF ..
+cmake --build . --config Release -j %NUMBER_OF_PROCESSORS%
+cmake --build . --config Release --target install
+popd
+
+mkdir win-bin-x64-gpu
+pushd win-bin-x64-gpu
+cmake -T "v141,host=x64" -A "x64" ^
+  -DCMAKE_INSTALL_PREFIX=install ^
+  -DCMAKE_BUILD_TYPE=Release -DOCR_OUTPUT="BIN" -DOCR_VULKAN=ON ..
+cmake --build . --config Release -j %NUMBER_OF_PROCESSORS%
+cmake --build . --config Release --target install
+popd
+
+mkdir win-bin-x86-gpu
+pushd win-bin-x86-gpu
+cmake -T "v141,host=x64" -A "Win32" ^
+  -DCMAKE_INSTALL_PREFIX=install ^
+  -DCMAKE_BUILD_TYPE=Release -DOCR_OUTPUT="BIN" -DOCR_VULKAN=ON ..
 cmake --build . --config Release -j %NUMBER_OF_PROCESSORS%
 cmake --build . --config Release --target install
 popd
